db309a4f839f70daa33a261e414c114f5e3f9c6a
daccatime <dawsonmcnally@hotmail.com>
2021-02-18 10:36:22 +0000

    first initial cryptodash

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React, {Component} from 'react';
+
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+    <Welcome name= {"crytodash"} />
+  );
+    }
+}
+
+export default App;
